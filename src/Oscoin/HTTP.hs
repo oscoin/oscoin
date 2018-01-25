4c068bf2f0b5b2e5e126a9b5246fb14bb9e59c8d
Alexis Sellier <alexis@monadic.xyz>
2018-01-25 15:09:17 +0100

    Think about the Environment

--- src/Oscoin/HTTP.hs
+++ src/Oscoin/HTTP.hs
@@ -9,20 +9,29 @@ import           Oscoin.HTTP.Internal
 import           Web.Spock
 import           Web.Spock.Config
 import           Data.Aeson ((.=), object)
+import qualified Network.Wai as Wai
 import           Network.Wai.Middleware.Static ((>->))
 import qualified Network.Wai.Middleware.Static as Wai
 import qualified Network.Wai.Middleware.RequestLogger as Wai
 
-run :: Int -> IO ()
-run port = do
+data Environment = Production | Development | Testing
+    deriving (Show)
+
+run :: Environment -> Int -> IO ()
+run env port = do
     spockCfg <- defaultSpockCfg () PCNoDatabase (State ())
-    runSpock port (spock spockCfg app)
+    runSpock port (spock spockCfg (app env))
+
+loggingMiddleware :: Environment -> Wai.Middleware
+loggingMiddleware Production = Wai.logStdout
+loggingMiddleware Development = Wai.logStdoutDev
+loggingMiddleware Testing = id
 
 -- | Entry point for the Spock application.
-app :: Api ()
-app = do
-    middleware $
-        Wai.logStdoutDev . Wai.staticPolicy (Wai.noDots >-> Wai.addBase ".")
+app :: Environment -> Api ()
+app env = do
+    middleware $ loggingMiddleware env
+               . Wai.staticPolicy (Wai.noDots >-> Wai.addBase ".")
 
     get root $ do
         json $ object [ "ok" .= True ]
