ccb95d678d606834d79570ff49573a1444b94ee6
Alexis Sellier <alexis@monadic.xyz>
2018-01-31 13:42:13 +0100

    Document http test helpers module

--- test/Oscoin/HTTP/Test/Helpers.hs
+++ test/Oscoin/HTTP/Test/Helpers.hs
@@ -20,6 +20,7 @@ import qualified Network.HTTP.Types.Header as HTTP
 -- | Like "Assertion" but bound to a user session (cookies etc.)
 type Session = Wai.Session
 
+-- | Turn a "Session" into an "Assertion".
 runSession :: Session () -> Assertion
 runSession sess =
     spockAsApp (mkMiddleware Testing) >>= Wai.runSession sess
@@ -28,18 +29,21 @@ runSession sess =
 assertBody :: Aeson.ToJSON a => a -> Wai.SResponse -> Wai.Session ()
 assertBody obj = Wai.assertBody (Aeson.encode obj)
 
+-- | Assert a response status is 200 OK.
 assertOK :: Wai.SResponse -> Wai.Session ()
 assertOK = Wai.assertStatus 200
 
+-- | Assert a response status is the given code.
 assertStatus :: Int -> Wai.SResponse -> Wai.Session ()
 assertStatus = Wai.assertStatus
 
+-- | Low-level HTTP request helper.
 request
     :: Aeson.ToJSON a
-    => HTTP.StdMethod
-    -> ByteString
-    -> HTTP.RequestHeaders
-    -> Maybe a
+    => HTTP.StdMethod                    -- ^ Request method
+    -> ByteString                        -- ^ Request path
+    -> HTTP.RequestHeaders               -- ^ Request headers
+    -> Maybe a                           -- ^ Request body
     -> Wai.Session Wai.SResponse
 request method path headers body =
     Wai.srequest $ Wai.SRequest (Wai.setPath req path) (reqBody body)
@@ -51,16 +55,20 @@ request method path headers body =
     reqBody Nothing    = LBS.empty
     reqBody (Just obj) = Aeson.encode obj
 
+-- | A GET request.
 get :: ByteString -> Wai.Session Wai.SResponse
 get path = request GET path [] noBody
 
+-- | A DELETE request.
 delete :: ByteString -> Wai.Session Wai.SResponse
 delete path = request DELETE path [] noBody
 
+-- | A PUT request.
 put :: Aeson.ToJSON a => ByteString -> a -> Wai.Session Wai.SResponse
 put path body =
     request PUT path [] (Just body)
 
+-- | A POST request.
 post :: Aeson.ToJSON a => ByteString -> a -> Wai.Session Wai.SResponse
 post path body =
     request POST path [] (Just body)
@@ -69,5 +77,6 @@ post path body =
 noBody :: Maybe ()
 noBody = Nothing
 
+-- | Helper for string literals.
 t :: Text -> Text
 t = id
