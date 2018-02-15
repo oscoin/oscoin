f54983b5d2b8a4e5a78aa656e9395d2497c3befa
Alexis Sellier <alexis@monadic.xyz>
2018-02-15 16:01:02 +0100

    Add 'assertError' function

--- test/Oscoin/Test/Helpers.hs
+++ test/Oscoin/Test/Helpers.hs
@@ -8,3 +8,7 @@ import           Test.Tasty.HUnit
 assertNoError :: Either Error a -> Assertion
 assertNoError (Left err) = assertFailure (T.unpack $ "Error: " <> fromError err)
 assertNoError (Right _)  = pass
+
+assertError :: Either Error a -> Assertion
+assertError (Right _) = assertFailure "Expected error, but got none"
+assertError (Left _)  = pass
