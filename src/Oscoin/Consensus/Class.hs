6da62ab2b4a02e017c6da7907fc67eb738c8d84a
Alexis Sellier <alexis@monadic.xyz>
2018-04-13 22:50:49 +0200

    Indent file properly

--- src/Oscoin/Consensus/Class.hs
+++ src/Oscoin/Consensus/Class.hs
@@ -3,25 +3,25 @@ module Oscoin.Consensus.Class where
 import           Oscoin.Prelude
 
 class Context m where
-  type T m
-  type Key m
+    type T m
+    type Key m
 
-  get :: Key m -> m (Maybe ByteString)
-  set :: Key m -> ByteString -> m (T m)
-  del :: Key m -> m (T m)
+    get :: Key m -> m (Maybe ByteString)
+    set :: Key m -> ByteString -> m (T m)
+    del :: Key m -> m (T m)
 
 type Score = [ByteString]
 
 class View m where
-  type Transaction m
-  type BlockHeader m
+    type Transaction m
+    type BlockHeader m
 
-  apply :: Maybe (BlockHeader m) -> [Transaction m] -> m ()
+    apply :: Maybe (BlockHeader m) -> [Transaction m] -> m ()
 
 class (Ord (Tick a), Num (Tick a)) => Protocol a where
-  type Msg a
-  type Addr a
-  type Tick a
+    type Msg a
+    type Addr a
+    type Tick a
 
-  step :: a -> Tick a -> Maybe (Addr a, Msg a) -> (a, [(Addr a, Msg a)])
-  epoch :: a -> Tick a
+    step :: a -> Tick a -> Maybe (Addr a, Msg a) -> (a, [(Addr a, Msg a)])
+    epoch :: a -> Tick a
