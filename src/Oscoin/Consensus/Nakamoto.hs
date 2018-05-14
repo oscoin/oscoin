8bb20454210e759d0f58b84d82bec56f3ca8fcfd
Alexis Sellier <alexis@monadic.xyz>
2018-05-14 15:35:20 +0200

    Add a bunch of functions for Nakamoto consensus

---
+++ src/Oscoin/Consensus/Nakamoto.hs
@@ -0,0 +1,56 @@
+module Oscoin.Consensus.Nakamoto where
+
+import           Oscoin.Prelude
+
+import           Oscoin.Crypto.Blockchain
+import           Oscoin.Crypto.Blockchain.Block
+import           Oscoin.Crypto.Hash (hash)
+
+import           Crypto.Number.Serialize (os2ip)
+import qualified Data.List.NonEmpty as NonEmpty
+
+-- | Block difficulty.
+type Difficulty = Integer
+
+blockDifficulty :: BlockHeader -> Difficulty
+blockDifficulty = notImplemented
+
+-- | Calculate block difficulty.
+difficulty :: BlockHeader -> Difficulty
+difficulty = os2ip . hash
+
+-- | The minimum difficulty.
+minDifficulty :: Difficulty
+minDifficulty =
+    0x0FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF
+
+-- | The difficulty at genesis.
+genesisDifficulty :: Difficulty
+genesisDifficulty =
+    0x00000000FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF
+    -- ^ This is the original difficulty of Bitcoin at genesis.
+
+-- | Return whether or not a block has a valid difficulty.
+isDifficultyValid :: BlockHeader -> Bool
+isDifficultyValid header =
+    difficulty header < blockDifficulty header
+
+-- | Calculate the difficulty of a blockchain.
+chainDifficulty :: Blockchain tx -> Difficulty
+chainDifficulty (Blockchain blks) =
+    if   length range < blocksConsidered
+    then genesisDifficulty
+    else currentDifficulty * targetElapsed `div` toInteger actualElapsed
+  where
+    range             = NonEmpty.take blocksConsidered blks
+    rangeStart        = blockHeader $ NonEmpty.last (NonEmpty.head blks :| tail range)
+    rangeEnd          = blockHeader $ NonEmpty.head blks
+    actualElapsed     = blockTimestamp rangeEnd - blockTimestamp rangeStart
+    targetElapsed     = fromIntegral $ blocksConsidered * blockTimeSeconds
+    blocksConsidered  = timePeriodMinutes `div` blockTimeMinutes
+    timePeriodMinutes = timePeriodDays * 24 * 60
+    timePeriodDays    = 2 * 7 -- Two weeks.
+    blockTimeMinutes  = 10
+    blockTimeSeconds  = blockTimeMinutes * 60
+    currentDifficulty = blockDifficulty rangeEnd
+    genesisDifficulty = blockDifficulty . blockHeader $ NonEmpty.last blks
