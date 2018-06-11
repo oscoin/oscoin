947be8eb5e9485804d31963b09e49ab1f46c47dd
Alexis Sellier <alexis@monadic.xyz>
2018-06-11 22:17:41 +0200

    Properly test message amplification

--- test/Oscoin/Consensus/Tests.hs
+++ test/Oscoin/Consensus/Tests.hs
@@ -2,7 +2,6 @@ module Oscoin.Consensus.Tests (tests) where
 
 import           Oscoin.Prelude hiding (log)
 
-import           Oscoin.Consensus.Class
 import           Oscoin.Consensus.Test.Network
 import           Oscoin.Consensus.Test.Network.Arbitrary
 import           Oscoin.Consensus.Test.Node
@@ -21,51 +20,65 @@ tests :: [TestTree]
 tests =
     [ testGroup "With Partitions"
         [ testProperty "All nodes DON'T include all txns (simple test)" $
-            expectFailure $ propNetworkNodesConverge nodesMatch (arbitraryNetwork @(TestNode DummyTx))
+            expectFailure $ propNetworkNodesConverge nodesMatch anyAmp (arbitraryNetwork @(TestNode DummyTx))
         , testProperty "All nodes DON'T include all txns (buffered test)" $
-            expectFailure $ propNetworkNodesConverge nodesMatch (arbitraryNetwork @(BufferedTestNode DummyTx))
+            expectFailure $ propNetworkNodesConverge nodesMatch anyAmp (arbitraryNetwork @(BufferedTestNode DummyTx))
         , testProperty "All nodes include all txns (simple fault tolerant)" $
-            propNetworkNodesConverge nodePrefixesMatch (arbitraryPartitionedNetwork @(SimpleNode DummyTx))
+            propNetworkNodesConverge nodePrefixesMatch (<= 2) (arbitraryPartitionedNetwork @(SimpleNode DummyTx))
         ]
     , testGroup "Without Partitions"
         [ testProperty "All nodes include all txns (simple test)" $
-            propNetworkNodesConverge nodesMatch (arbitraryHealthyNetwork @(TestNode DummyTx))
+            propNetworkNodesConverge nodesMatch anyAmp (arbitraryHealthyNetwork @(TestNode DummyTx))
         , testProperty "All nodes include all txns (buffered test)" $
-            propNetworkNodesConverge nodesMatch (arbitraryHealthyNetwork @(BufferedTestNode DummyTx))
+            propNetworkNodesConverge nodesMatch anyAmp (arbitraryHealthyNetwork @(BufferedTestNode DummyTx))
         , testProperty "All nodes include all txns (simple fault tolerant)" $
-            propNetworkNodesConverge nodePrefixesMatch (arbitraryHealthyNetwork @(SimpleNode DummyTx))
+            propNetworkNodesConverge nodePrefixesMatch (<= 2) (arbitraryHealthyNetwork @(SimpleNode DummyTx))
         , testProperty "All nodes include all txns (nakamoto)" $
-            propNetworkNodesConverge nodePrefixesMatch (arbitraryHealthyNetwork @(Nakamoto DummyTx))
+            propNetworkNodesConverge nodePrefixesMatch (<= 3) (arbitraryHealthyNetwork @(Nakamoto DummyTx))
         ]
     ]
+  where
+    anyAmp = const True
 
 propNetworkNodesConverge
     :: forall a . (Show (TestNetwork a), TestableNode a)
     => (TestNetwork a -> Bool)
+    -> (Int -> Bool)
     -> Gen (TestNetwork a)
     -> Property
-propNetworkNodesConverge stateCmp testNetworks =
+propNetworkNodesConverge stateCmp msgAmpPred testNetworks =
     forAllShrink testNetworks shrink $ \tn ->
         networkNonTrivial tn ==>
-            let tn'                  = runNetwork tn
-                filteredInitialMsgs  = Set.filter isMsg (tnMsgs tn)
-                msgAmp               = tnMsgCount tn' `div` length filteredInitialMsgs
+            let tn'           = runNetwork tn
+                msgAmp        = msgAmplification tn' msgsSent
+                initialMsgs   = Set.filter isMsg (tnMsgs tn)
+                msgsSent      = tnMsgCount tn' - length initialMsgs
 
-             in cover (not . null . testableIncludedTxs . head . toList $ tnNodes tn') 90 "replicated any data" $
-                 counterexample (prettyCounterexample tn' maximumMsgAmp)
-                                (stateCmp (tnNodes tn') && msgAmp <= maximumMsgAmp)
+             in cover (not . null . testableIncludedTxs . head . toList $ tnNodes tn') 60 "replicated any data" $
+                 counterexample (prettyCounterexample tn' msgAmp msgsSent)
+                                (stateCmp tn' && msgAmpPred msgAmp)
+
+msgAmplification :: TestableNode a => TestNetwork a -> Int -> Int
+msgAmplification tn@TestNetwork{..} msgsSent =
+    if expectedMsgs == 0 then 0 else msgsSent `div` expectedMsgs
   where
-    maximumMsgAmp = 9000
+    expectedMsgs = nodes * prefix
+    nodes        = length tnNodes
+    prefix       = length $ commonPrefix $ nodePrefixes tn
 
-prettyCounterexample :: TestableNode a => TestNetwork a -> Int -> String
-prettyCounterexample TestNetwork{..} maxMsgAmp =
-    prettyLog ++ prettyNodes ++ prettyStates ++ prettyInfo ++ prettyMsgAmp
+prettyCounterexample :: TestableNode a => TestNetwork a -> Int -> Int -> String
+prettyCounterexample tn@TestNetwork{..} msgAmp msgsSent =
+    prettyLog ++ prettyNodes ++ prettyInfo ++ prettyMsgAmp
   where
     prettyLog    = unlines $  " log:" : reverse ["  " ++ show l | l <- reverse $ sort tnLog]
-    prettyStates = unlines $ [" states:", "  " ++ show (map testablePostState tnNodes)]
     prettyNodes  = unlines $ [" nodes:", "  " ++ show (length tnNodes)]
     prettyInfo   = unlines $ [" info:", unlines ["  " ++ show (testableNodeAddr n) ++ ": " ++ testableShow n | n <- toList tnNodes]]
-    prettyMsgAmp = unlines $ [" message amplification: " ++ show maxMsgAmp]
+    prettyMsgAmp = unlines $ [" msgs sent: "     ++ show msgsSent,
+                              " common prefix: " ++ show (length $ commonPrefix $ nodePrefixes tn),
+                              " amplification: " ++ show msgAmp ++ "x" ]
+
+nodesMatch :: TestableNode a => TestNetwork a -> Bool
+nodesMatch TestNetwork{..} = equal $ map testablePostState (toList tnNodes)
 
 nodePrefixesMatch :: TestableNode a => TestNetwork a -> Bool
 nodePrefixesMatch tn =
