5bc4c5736bffe83951772021642633d29f5db7cb
Alexis Sellier <alexis@cloudhead.io>
2018-08-27 16:52:08 +0200

    Implement encoding agnostic HTTP helpers

--- src/Oscoin/HTTP/Internal.hs
+++ src/Oscoin/HTTP/Internal.hs
@@ -6,9 +6,11 @@ import           Oscoin.Environment
 import qualified Oscoin.Node as Node
 
 import           Codec.Serialise (Serialise, serialise)
-import           Data.Aeson (ToJSON, (.=))
+import qualified Codec.Serialise as Serialise
+import           Data.Aeson (FromJSON, ToJSON, (.=))
 import qualified Data.Aeson as Aeson
 import qualified Data.ByteString.Lazy as LBS
+import qualified Data.Text as T
 import qualified Network.HTTP.Types.Status as HTTP
 import qualified Network.Wai as Wai
 import qualified Network.Wai.Middleware.RequestLogger as Wai
@@ -40,11 +42,22 @@ mkState = State ()
 getHeader :: Text -> ApiAction tx s i (Maybe Text)
 getHeader = Spock.header
 
-getBody :: Aeson.FromJSON a => ApiAction tx s i (Maybe a)
-getBody = Spock.jsonBody
+getHeader' :: Text -> ApiAction tx s i Text
+getHeader' h = do
+    mh <- getHeader h
+    case mh of
+        Just v  -> pure v
+        Nothing -> respond HTTP.badRequest400
 
 getRawBody :: ApiAction tx s i LBS.ByteString
 getRawBody = LBS.fromStrict <$> Spock.body
+
+getAccept :: ApiAction tx s i Text
+getAccept = do
+    result <- getHeader "Accept"
+    case result of
+        Nothing     -> respond HTTP.notAcceptable406
+        Just accept -> pure accept
 
 getState :: ApiAction tx s i State
 getState = Spock.getState
@@ -61,18 +74,44 @@ respondJson status body = do
     Spock.setHeader "Content-Type" "application/json"
     respondBytes status (Aeson.encode body)
 
-respond :: HTTP.Status -> ApiAction tx s i ()
-respond = Spock.setStatus
+respond :: HTTP.Status -> ApiAction tx s i a
+respond status = respondBytes status mempty
 
-respondCbor :: Serialise a => HTTP.Status -> a -> ApiAction tx s i ()
+respondCbor :: Serialise a => HTTP.Status -> a -> ApiAction tx s i b
 respondCbor status body = do
     Spock.setHeader "Content-Type" "application/cbor"
     respondBytes status (serialise body)
 
-respondBytes :: HTTP.Status -> LBS.ByteString -> ApiAction tx s i ()
+respondBytes :: HTTP.Status -> LBS.ByteString -> ApiAction tx s i a
 respondBytes status body = do
     Spock.setStatus status
     Spock.lazyBytes body
+
+respondBody :: (ToJSON a, Serialise a) => a -> ApiAction tx s i ()
+respondBody a = do
+    accept <- getAccept
+    Spock.setHeader "Content-Type" accept
+    case encode accept a of
+        Nothing -> respond HTTP.notAcceptable406
+        Just bs -> respondBytes HTTP.accepted202 bs
+  where
+    encode :: (Serialise a, ToJSON a) => Text -> a -> Maybe LBS.ByteString
+    encode "application/json" = Just . Aeson.encode
+    encode "application/cbor" = Just . Serialise.serialise
+    encode _                  = const Nothing
+
+getBody :: (Serialise a, FromJSON a) => ApiAction tx s i a
+getBody = do
+    ct   <- getHeader' "Content-Type"
+    body <- getRawBody
+    case decode ct body of
+        Left l  -> respond HTTP.badRequest400
+        Right a -> pure a
+  where
+    decode :: (Serialise a, FromJSON a) => Text -> LBS.ByteString -> Either Text a
+    decode "application/json" bs = first T.pack          (Aeson.eitherDecode' bs)
+    decode "application/cbor" bs = first (T.pack . show) (Serialise.deserialiseOrFail bs)
+    decode contentType        _  = Left $ "unknown content type " ++ contentType
 
 notImplemented :: ApiAction tx s i ()
 notImplemented =
