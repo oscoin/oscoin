bdb1ab72715a8195be53b19c4c672db6b7ce30a8
Tomás Senart <tomas@monadic.xyz>
2018-09-06 17:33:06 +0200

    API: FromJSON instance of GetTxResponse

--- src/Oscoin/API/HTTP/Response.hs
+++ src/Oscoin/API/HTTP/Response.hs
@@ -6,7 +6,7 @@ import           Oscoin.API.HTTP.Internal (ApiTx)
 import           Oscoin.Crypto.Hash (Hashed)
 import           Oscoin.Crypto.Blockchain.Block (BlockHash)
 
-import           Data.Aeson (ToJSON)
+import           Data.Aeson (FromJSON, ToJSON)
 import           Codec.Serialise (Serialise)
 
 -- | Response type for the GET /transactions/:id API endpoint.
@@ -18,4 +18,5 @@ data GetTxResponse = GetTxResponse
     } deriving (Show, Eq, Generic)
 
 instance ToJSON GetTxResponse
+instance FromJSON GetTxResponse
 instance Serialise GetTxResponse
