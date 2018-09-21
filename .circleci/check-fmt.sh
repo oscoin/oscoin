a8349212891fe1ccc8a0fcf79e445bc88db97757
Tomás Senart <tomas@monadic.xyz>
2018-09-21 12:57:39 +0200

    CI: Show misformatted files

--- .circleci/check-fmt.sh
+++ .circleci/check-fmt.sh
@@ -2,13 +2,12 @@
 
 set -euo pipefail
 
-function format() {
-  echo "Formatting $1"
-  stack exec -- stylish-haskell --inplace "$1" || echo "Error formatting: $1" && true
-}
+stack exec -- stylish-haskell **/**.hs --inplace
+modified=$(git diff --name-only)
+count=$(echo -n "$modified" | wc -l)
 
-export -f format
+if [[ $count -ne 0 ]]; then
+  printf "Found %d misformatted files:\n%s" $count $modified
+fi
 
-git ls-files | grep '\.l\?hs$' | xargs -P8 -n1 bash -c 'format "$@"' _
-
-exit $(git ls-files --modified | wc -l)
+exit $count
