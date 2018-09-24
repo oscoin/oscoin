0dc1c9bcc636df17918715656491a57e24013f96
Alexis Sellier <alexis@monadic.xyz>
2018-09-24 13:24:57 +0200

    Create HelperT with access to API client

--- src/Oscoin/Git/Remote/Helper.hs
+++ src/Oscoin/Git/Remote/Helper.hs
@@ -1,6 +1,31 @@
-module Oscoin.Git.Remote.Helper where
+module Oscoin.Git.Remote.Helper
+    ( runRemoteHelper
+    ) where
 
 import           Oscoin.Prelude
 
+import qualified Oscoin.API.Client as API
+import           Oscoin.API.HTTP.Client (HttpClientT, runHttpClientT)
+import           Oscoin.CLI.KeyStore (MonadKeyStore)
+
+import           Crypto.Random.Types (MonadRandom(..))
+
+-- | The remote helper monad stack.
+newtype HelperT m a = HelperT { runHelperT :: HttpClientT m a }
+    deriving (Functor, Applicative, Monad, MonadTrans, API.MonadClient)
+
+instance MonadKeyStore m => MonadKeyStore (HelperT m)
+
+instance MonadRandom m => MonadRandom (HelperT m) where
+    getRandomBytes = lift . getRandomBytes
+
+--------------------------------------------------------------------------------
+
 runRemoteHelper :: IO ()
-runRemoteHelper = pure ()
+runRemoteHelper =
+    runHttpClientT nodeAddr $ runHelperT $ remoteHelper
+  where
+    nodeAddr = "http://127.0.0.1:8080"
+
+remoteHelper :: API.MonadClient m => m ()
+remoteHelper = pure ()
