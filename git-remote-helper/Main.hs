845baeaf849082006d667dc65ffb6f3a94e35b47
Alexis Sellier <alexis@monadic.xyz>
2018-09-24 12:43:06 +0200

    Start git-remote-helper skeleton

---
+++ git-remote-helper/Main.hs
@@ -0,0 +1,8 @@
+module Main (main) where
+
+import           Oscoin.Prelude
+
+import           Oscoin.Git.Remote.Helper
+
+main :: IO ()
+main = runRemoteHelper
---
+++ src/Oscoin/Git/Remote/Helper.hs
@@ -0,0 +1,6 @@
+module Oscoin.Git.Remote.Helper where
+
+import           Oscoin.Prelude
+
+runRemoteHelper :: IO ()
+runRemoteHelper = pure ()
