bc87d0056a89516dc74d41d5d16d748a7a5870c9
Thomas Scholtes <thomas@monadic.xyz>
2018-10-02 12:16:29 +0200

    Define 'Rad.txEval' and type aliases
    This commit consolidates code in the `Evaluator.Radicle` module and
    makes code in `node/Main` less generic.

--- node/Main.hs
+++ node/Main.hs
@@ -9,6 +9,7 @@ import           Oscoin.Clock
 import qualified Oscoin.Consensus as Consensus
 import           Oscoin.Consensus.BlockStore (genesisBlockStore)
 import           Oscoin.Consensus.Evaluator (fromEvalError)
+import qualified Oscoin.Consensus.Evaluator.Radicle as Rad
 import           Oscoin.Crypto.Blockchain (Difficulty)
 import           Oscoin.Crypto.Blockchain.Block (genesisBlock)
 import           Oscoin.Data.Tx (createTx)
@@ -24,11 +25,9 @@ import qualified Oscoin.P2P as P2P
 import           Oscoin.Storage (hoistStorage)
 import qualified Oscoin.Storage.Block as BlockStore
 
-import qualified Oscoin.Consensus.Evaluator.Radicle as Rad
 
 import qualified Control.Concurrent.Async as Async
 import           Control.Monad.Except (ExceptT(..), runExceptT, withExceptT)
-import           Data.Default (def)
 import qualified Data.Text as T
 import qualified Data.Yaml as Yaml
 import           GHC.Generics (Generic)
@@ -88,13 +87,12 @@ main = do
     Args{..} <- execParser args
 
     let consensus = Consensus.nakamotoConsensus difficulty
-    let eval      = Node.defaultEval
 
     keys     <- readKeyPair
     nid      <- pure (mkNodeId $ fst keys)
     mem      <- Mempool.newIO
-    stree    <- STree.new def
-    gen      <- either die pure =<< genesisFromPath eval prelude keys
+    stree    <- STree.new Rad.pureEnv
+    gen      <- either die pure =<< genesisFromPath prelude keys
     blkStore <- BlockStore.newIO $ genesisBlockStore gen
     seeds'   <- Yaml.decodeFileThrow seeds
 
@@ -104,7 +102,7 @@ main = do
                    mem
                    stree
                    blkStore
-                   eval
+                   Rad.txEval
                    consensus                                      $ \nod ->
         withAPI    Testing                                        $ \api ->
         withGossip lgr
@@ -124,11 +122,11 @@ main = do
         , Node.cfgLogger = lgr
         }
 
-    genesisFromPath eval path kp = runExceptT $ do
+    genesisFromPath path kp = runExceptT $ do
         val <- ExceptT $ Rad.parseValue (T.pack path) <$> readFile path
         withExceptT (T.unlines . map fromEvalError) . ExceptT $ do
             tx <- liftIO $ createTx kp val
-            pure $ genesisBlock def eval epoch [tx]
+            pure $ genesisBlock Rad.pureEnv Rad.txEval epoch [tx]
 
     miner nod gos = runGossipT gos . runNodeT nod $ Node.miner
     storage nod   = hoistStorage (runNodeT nod) Node.storage
