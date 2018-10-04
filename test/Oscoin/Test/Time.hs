963befb2b0310c74fa5e6b70d61c42f9259b7c6d
Tomás Senart <tomas@monadic.xyz>
2018-10-04 19:25:27 +0200

    Add missing test file

---
+++ test/Oscoin/Test/Time.hs
@@ -0,0 +1,12 @@
+{-# OPTIONS_GHC -fno-warn-orphans #-}
+
+module Oscoin.Test.Time where
+
+import           Oscoin.Prelude
+import           Oscoin.Time
+
+import           Test.QuickCheck
+import           Test.QuickCheck.Instances ()
+
+instance Arbitrary Timestamp where
+    arbitrary = fromEpoch <$> arbitrary
