40d11d1270060937237c62cac38734683e753721
Thomas Scholtes <thomas@monadic.xyz>
2018-10-09 10:01:59 +0200

    Test client uses code from real HTTP client
    We generalize `HttpClientT` so that the test client can use most of its
    logic. This reduces the code duplication between the two clients.

--- src/Oscoin/API/HTTP/Client.hs
+++ src/Oscoin/API/HTTP/Client.hs
@@ -1,15 +1,21 @@
-{-# LANGUAGE UndecidableInstances #-}
-
--- | Implementation of 'MonadClient' using the HTTP API.
--- @
---      foo :: MonadClient m => m a
+-- | Implementation of 'MonadClient' using the HTTP API and
+-- "Network.Http.Client".
 --
---      main = runHttpClient "http://localhost:8080" foo
 -- @
+-- foo :: 'MonadClient' m => m a
+-- main = 'runHttpClientT' "http://localhost:8080" foo
+-- @
+--
+-- 'runHttpClientT'' allows you to use a custom request function.
 module Oscoin.API.HTTP.Client
-    ( MonadClient(..)
+    ( runHttpClientT
     , HttpClientT
-    , runHttpClientT
+    , MonadClient(..)
+
+    -- * Custom HTTP client
+    , runHttpClientT'
+    , Request(..)
+    , Response(..)
     ) where
 
 import           Oscoin.Prelude hiding (get)
@@ -19,23 +25,33 @@ import           Oscoin.API.Types
 import           Oscoin.Crypto.Hash (fromHashed)
 
 import           Codec.Serialise
-import qualified Data.ByteString.Lazy as LBS
 import qualified Data.Text as T
-import           Network.HTTP.Client hiding (Proxy)
+import qualified Network.HTTP.Client as Client
 import           Network.HTTP.Types.Header
 import           Network.HTTP.Types.Method
+import           Network.HTTP.Types.Status
 import           Web.HttpApiData (toUrlPiece)
 
 
-data HttpClient = HttpClient
-    { httpClientBaseRequest :: Request
-    , httpClientManager     :: Manager
+type Requester m = Request -> m Response
+
+data Request = Request
+    { requestMethod  :: Method
+    , requestPath    :: Text
+    , requestHeaders :: RequestHeaders
+    , requestBody    :: LByteString
     }
 
+data Response = Response
+    { responseBody   :: LByteString
+    , responseStatus :: Status
+    }
 
-newtype HttpClientT m a = HttpClientT (ReaderT HttpClient m a)
-    deriving (Functor, Applicative, Monad, MonadMask, MonadCatch, MonadThrow, MonadTrans, MonadIO)
+newtype HttpClientT m a = HttpClientT (ReaderT (Requester m) m a)
+    deriving (Functor, Applicative, Monad, MonadMask, MonadCatch, MonadThrow, MonadIO)
 
+instance MonadTrans HttpClientT where
+    lift ma = HttpClientT $ lift ma
 
 instance (Monad m, MonadIO m) => MonadClient (HttpClientT m) where
     submitTransaction tx =
@@ -47,49 +63,63 @@ instance (Monad m, MonadIO m) => MonadClient (HttpClientT m) where
     getState key =
         get $ "/state?q=[" <> T.intercalate "," key <> "]"
 
-type Uri = String
 
 -- | @runHttpClientT uri@ throws an exception if @uri@ is not a valid URI.
-runHttpClientT :: (MonadIO m) => Uri -> HttpClientT m a -> m a
+runHttpClientT :: (MonadIO m) => Text -> HttpClientT m a -> m a
 runHttpClientT uri (HttpClientT m) = do
-    client <- liftIO $ createHttpClient uri
+    client <- newHttpClient uri
     runReaderT m client
 
+runHttpClientT' :: (Request -> m Response) -> HttpClientT m a -> m a
+runHttpClientT' mkRequest (HttpClientT m) = runReaderT m mkRequest
 
-createHttpClient :: Uri -> IO HttpClient
-createHttpClient uri = do
-    manager <- newManager defaultManagerSettings
-    baseRequest <- parseRequest uri
-    pure HttpClient { httpClientBaseRequest = baseRequest
-                    , httpClientManager = manager
-                    }
+------------------------------------------------------------------------------
+
+newHttpClient :: (MonadIO m) => Text -> m (Requester m)
+newHttpClient uri = liftIO $ do
+    manager <- Client.newManager Client.defaultManagerSettings
+    baseRequest <- Client.parseRequest (toS uri)
+    pure $ liftIO . makeHttpRequest manager baseRequest
+
+makeHttpRequest :: Client.Manager -> Client.Request -> Request -> IO Response
+makeHttpRequest manager baseRequest Request{..} = do
+    let req = baseRequest
+            { Client.method = methodGet
+            , Client.path = encodeUtf8 requestPath
+            , Client.requestHeaders = requestHeaders
+            , Client.requestBody = Client.RequestBodyLBS $ requestBody
+            }
+    res <- Client.httpLbs req manager
+    pure $ Response
+        { responseStatus = Client.responseStatus res
+        , responseBody = Client.responseBody res
+        }
 
 
-makeRequest :: (MonadIO m, Serialise a) => (Request -> Request) -> HttpClientT m (Result a)
-makeRequest buildRequest = HttpClientT $ do
-    HttpClient{..} <- ask
-    let req = buildRequest httpClientBaseRequest
-    deserialiseResponse <$> liftIO (httpLbs req httpClientManager)
+makeRequest :: forall a m. (Serialise a, MonadIO m) => Request -> HttpClientT m (Result a)
+makeRequest req = HttpClientT $ do
+    mkRequest <- ask
+    Response{..} <- lift $ mkRequest req
+    pure $ case deserialiseOrFail $ responseBody of
+        Left _    -> Err "Failed to deserialise response"
+        Right val -> val
 
 get :: (MonadIO m, Serialise a) => Text -> HttpClientT m (Result a)
 get reqPath =
-    makeRequest $ \req ->
-        req { method = methodGet
-            , path = encodeUtf8 reqPath
+    makeRequest
+        Request
+            { requestMethod = methodGet
+            , requestPath = reqPath
             , requestHeaders = [(hAccept, "application/cbor")]
+            , requestBody = mempty
             }
 
 post :: (MonadIO m, Serialise a, Serialise b) => Text -> a ->  HttpClientT m (Result b)
 post reqPath reqBody =
-    makeRequest $ \req ->
-        req { method = methodPost
-            , path = encodeUtf8 reqPath
+    makeRequest
+        Request
+            { requestMethod = methodPost
+            , requestPath = reqPath
             , requestHeaders = [(hAccept, "application/cbor"), (hContentType, "application/cbor")]
-            , requestBody = RequestBodyLBS $ serialise reqBody
+            , requestBody = serialise reqBody
             }
-
-deserialiseResponse :: (Serialise a) => Response LBS.ByteString -> Result a
-deserialiseResponse response =
-    case deserialiseOrFail $ responseBody response of
-        Left _    -> Err "Failed to deserialise response"
-        Right val -> val
