c472b11db0ac6df2dc303d6d8335535220a84e53
Tomás Senart <tomas@monadic.xyz>
2018-10-10 15:22:14 +0200

    Format shell scripts with shfmt

--- scripts/ci.sh
+++ scripts/ci.sh
@@ -3,29 +3,29 @@
 set -euo pipefail
 
 function load-cache() {
-    bucket="gs://oscoin-build-cache"
-    key="stack-work-$(cat stack.yaml | sha256sum | cut -d ' ' -f 1)".tar.gz
-    gsutil -m cp -r "$bucket/v2/*" "$bucket/$key" . || true
-    for f in stack.tar.gz $key; do
-      if [[ -e $f ]]; then
-        tar xzf $f
-      fi
-    done
+  bucket="gs://oscoin-build-cache"
+  key="stack-work-$(cat stack.yaml | sha256sum | cut -d ' ' -f 1)".tar.gz
+  gsutil -m cp -r "$bucket/v2/*" "$bucket/$key" . || true
+  for f in stack.tar.gz $key; do
+    if [[ -e $f ]]; then
+      tar xzf $f
+    fi
+  done
 }
 
 function save-cache() {
-    bucket="gs://oscoin-build-cache"
-    key="stack-work-$(cat stack.yaml | sha256sum | cut -d ' ' -f 1)".tar.gz
-    if ! gsutil ls "$bucket/$key"; then
-      tar czf $key .stack-work
-      gsutil -m cp $key "$bucket/$key" || true
-    fi
+  bucket="gs://oscoin-build-cache"
+  key="stack-work-$(cat stack.yaml | sha256sum | cut -d ' ' -f 1)".tar.gz
+  if ! gsutil ls "$bucket/$key"; then
+    tar czf $key .stack-work
+    gsutil -m cp $key "$bucket/$key" || true
+  fi
 
-    rm -rf .stack/indices/Hackage/00-index.tar*
-    tar czf stack.tar.gz .stack
+  rm -rf .stack/indices/Hackage/00-index.tar*
+  tar czf stack.tar.gz .stack
 
-    if ! sha256sum --check stack.tar.gz.sha256; then
-      sha256sum stack.tar.gz > stack.tar.gz.sha256
-      gsutil -m cp stack.tar.gz* "$bucket/v2/" || true
-    fi
+  if ! sha256sum --check stack.tar.gz.sha256; then
+    sha256sum stack.tar.gz > stack.tar.gz.sha256
+    gsutil -m cp stack.tar.gz* "$bucket/v2/" || true
+  fi
 }
