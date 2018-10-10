25f3c8caeb21b105c9ac220a1fc4f16c2796a335
Tomás Senart <tomas@monadic.xyz>
2018-10-10 15:20:18 +0200

    CI: Check formatting of shell scripts with shfmt

--- scripts/check-fmt.sh
+++ scripts/check-fmt.sh
@@ -5,7 +5,7 @@ set -euo pipefail
 shopt -s globstar
 
 base=$(mktemp -d "/tmp/oscoin-base.XXXXX")
-for f in **/**.hs; do
+for f in **/**.{hs,sh}; do
   path=$base/$(dirname $f)
   mkdir -p $path
   cp $f $path
@@ -14,6 +14,15 @@ done
 formatted=$(mktemp -d "/tmp/oscoin-formatted.XXXXX")
 cp -R $base/* $formatted
 
+# Haskell
 stack exec -- stylish-haskell $formatted/**/**.hs --inplace
+
+# Shell scripts
+# -s    simplify the code
+# -i 2  indent with two spaces
+# -bn   binary ops like && and | may start a line
+# -sr   redirect operators will be followed by a space
+# -w    write result to file instead of stdout
+shfmt -w -s -i 2 -bn -sr -l .
 
 diff -rq $base $formatted
