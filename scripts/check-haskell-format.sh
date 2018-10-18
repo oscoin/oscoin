39f07203b2162afdd0eb6c9d27a5a1e1e58ed238
Kim Altintop <kim@monadic.xyz>
2018-10-18 16:26:20 +0200

    CI: Do not use `stack exec` for linters
    Interferes with concurrent build steps.

--- scripts/check-haskell-format.sh
+++ scripts/check-haskell-format.sh
@@ -4,6 +4,8 @@ set -euo pipefail
 
 shopt -s globstar
 
+PATH=$HOME/.local/bin:$PATH
+
 base=$(mktemp -d "/tmp/oscoin-base.XXXXX")
 for f in **/**.hs; do
   path=$base/$(dirname "$f")
@@ -14,6 +16,6 @@ done
 formatted=$(mktemp -d "/tmp/oscoin-formatted.XXXXX")
 cp -R "$base"/* "$formatted"
 
-stack exec -- stylish-haskell "$formatted"/**/**.hs --inplace
+stylish-haskell "$formatted"/**/**.hs --inplace
 
 diff -rq "$base" "$formatted"
