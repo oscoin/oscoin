18facc10950946cda84ae52a582275885c408b6b
Alexis Sellier <alexis@monadic.xyz>
2018-10-31 10:46:22 +0100

    Extract Crypto.Data.Auth into its own package
    See oscoin/avl-auth

--- test/Main.hs
+++ test/Main.hs
@@ -4,7 +4,6 @@ import           Oscoin.Prelude
 
 import qualified Control.Concurrent.Tests as Concurrent
 import qualified Crypto.Test.Hash.Multi as Multihash
-import qualified Crypto.Tests as Crypto
 import qualified Data.Conduit.Tests as Conduit
 import qualified Oscoin.Tests as Oscoin
 import           Test.Tasty
@@ -12,7 +11,6 @@ import           Test.Tasty
 main :: IO ()
 main = defaultMain $ testGroup "All"
     [ Oscoin.tests
-    , Crypto.tests
     , Multihash.tests
     , Concurrent.tests
     , Conduit.tests
