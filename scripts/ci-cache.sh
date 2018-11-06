fd90bcfc81fff78de179d5aea4a5ccb19f945ce6
Kim Altintop <kim@monadic.xyz>
2018-11-06 17:02:51 +0100

    CI: Use our own haskell image
    Currently built from https://github.com/freebroccolo/docker-haskell, ie.
    the "official" haskell image on Docker Hub.

--- scripts/ci-cache.sh
+++ scripts/ci-cache.sh
@@ -5,7 +5,7 @@ set -euo pipefail
 function load-cache() {
   bucket="gs://oscoin-build-cache"
   key="stack-work-$(sha256sum < stack.yaml | cut -d ' ' -f 1)".tar.gz
-  gsutil -m cp -r "$bucket/v2/*" "$bucket/$key" . || true
+  gsutil -m cp -r "$bucket/v3/*" "$bucket/$key" . || true
   for f in stack.tar.gz "$key"; do
     if [[ -e $f ]]; then
       tar xzf "$f"
@@ -26,6 +26,6 @@ function save-cache() {
 
   if ! sha256sum --check stack.tar.gz.sha256; then
     sha256sum stack.tar.gz > stack.tar.gz.sha256
-    gsutil -m cp stack.tar.gz* "$bucket/v2/" || true
+    gsutil -m cp stack.tar.gz* "$bucket/v3/" || true
   fi
 }
