1447974e9f4c07a11cf6d092f6beeb48a421ea73
Alfredo Di Napoli <alfredo@monadic.xyz>
2019-03-14 10:50:33 +0100

    Allow multiple exe tests to be run in parallel

--- test/Integration/Test/Executable.hs
+++ test/Integration/Test/Executable.hs
@@ -8,6 +8,7 @@ import           Data.ByteString.Char8 as C8
 
 import           System.IO (Handle)
 import           System.IO.Temp
+import           System.Random (randomRIO)
 
 import           Test.Sandbox
 import           Test.Tasty
@@ -20,8 +21,10 @@ tests =
     ]
 
 
-withOscoinExe :: (Handle -> Handle -> Assertion) -> Assertion
-withOscoinExe f =
+withOscoinExe :: Int -> (Handle -> Handle -> Assertion) -> Assertion
+withOscoinExe gossipPort f = do
+    randomisedSpockPort <- randomRIO (9000, 10000)
+    randomisedEkgPort   <- randomRIO (8000, 8999)
     -- Generates a temporary directory where to store some ephemeral keys, which
     -- are needed for the test to pass on CI.
     withSystemTempDirectory "Oscoin.Ephemeral.Keys" $ \keyPath -> do
@@ -29,12 +32,22 @@ withOscoinExe f =
         withSandbox "oscoin-cli" ["keypair", "generate", "--keys", keyPath] defaultSandboxOptions $
             \_ _ -> pure ()
         -- Then, run the test.
-        withSandbox "oscoin" ["--api-port", "0", "--keys", keyPath, "--seed", "127.0.0.1:6942"] defaultSandboxOptions $
+        withSandbox "oscoin" [ "--api-port"
+                             , show (randomisedSpockPort :: Int)
+                             , "--gossip-port"
+                             , show gossipPort
+                             , "--keys"
+                             , keyPath
+                             , "--seed"
+                             , "127.0.0.1:" <> show gossipPort
+                             , "--ekg-port"
+                             , show (randomisedEkgPort :: Int)
+                             ] defaultSandboxOptions $
             \stdoutHandle stdErrHandle -> f stdoutHandle stdErrHandle
 
 testStartsOK :: Assertion
 testStartsOK =
-    withOscoinExe $ \stdoutHandle _stdErrHandle -> do
+    withOscoinExe 6942 $ \stdoutHandle _stdErrHandle -> do
         -- TODO(adn) In the future we want a better handshake string here.
         actual <- C8.hGet stdoutHandle 100
         assertBool ("oscoin started but gave unexpected output: " <> C8.unpack actual)
@@ -42,7 +55,7 @@ testStartsOK =
 
 testMinerOK :: Assertion
 testMinerOK =
-    withOscoinExe $ \stdoutHandle _stdErrHandle -> do
+    withOscoinExe 6943 $ \stdoutHandle _stdErrHandle -> do
         actual <- C8.hGet stdoutHandle 2000
         assertBool ("oscoin started but gave unexpected output: " <> C8.unpack actual)
                    ("mined block" `C8.isInfixOf` actual)
