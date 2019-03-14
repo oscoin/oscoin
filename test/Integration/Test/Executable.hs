c56b22f00081945ce84b18636e3b3151e901d6eb
Alfredo Di Napoli <alfredo@monadic.xyz>
2019-03-14 10:37:42 +0100

    Regen genesis after LTS bump.

--- test/Integration/Test/Executable.hs
+++ test/Integration/Test/Executable.hs
@@ -6,6 +6,7 @@ import           Oscoin.Prelude
 
 import           Data.ByteString.Char8 as C8
 
+import           System.IO (Handle)
 import           System.IO.Temp
 
 import           Test.Sandbox
@@ -14,11 +15,13 @@ import           Test.Tasty.HUnit
 
 tests :: [TestTree]
 tests =
-    [ testCase "it successfully starts" testStartsOK ]
+    [ testCase "it successfully starts" testStartsOK
+    , testCase "the miner produces blocks" testMinerOK
+    ]
 
 
-testStartsOK :: Assertion
-testStartsOK =
+withOscoinExe :: (Handle -> Handle -> Assertion) -> Assertion
+withOscoinExe f =
     -- Generates a temporary directory where to store some ephemeral keys, which
     -- are needed for the test to pass on CI.
     withSystemTempDirectory "Oscoin.Ephemeral.Keys" $ \keyPath -> do
@@ -27,8 +30,19 @@ testStartsOK =
             \_ _ -> pure ()
         -- Then, run the test.
         withSandbox "oscoin" ["--api-port", "0", "--keys", keyPath, "--seed", "127.0.0.1:6942"] defaultSandboxOptions $
-            \stdoutHandle _stdErrHandle -> do
-                -- TODO(adn) In the future we want a better handshake string here.
-                actual <- C8.hGet stdoutHandle 100
-                assertBool ("oscoin started but gave unexpected output: " <> C8.unpack actual)
-                           ("running in" `C8.isInfixOf` actual)
+            \stdoutHandle stdErrHandle -> f stdoutHandle stdErrHandle
+
+testStartsOK :: Assertion
+testStartsOK =
+    withOscoinExe $ \stdoutHandle _stdErrHandle -> do
+        -- TODO(adn) In the future we want a better handshake string here.
+        actual <- C8.hGet stdoutHandle 100
+        assertBool ("oscoin started but gave unexpected output: " <> C8.unpack actual)
+                   ("running in" `C8.isInfixOf` actual)
+
+testMinerOK :: Assertion
+testMinerOK =
+    withOscoinExe $ \stdoutHandle _stdErrHandle -> do
+        actual <- C8.hGet stdoutHandle 2000
+        assertBool ("oscoin started but gave unexpected output: " <> C8.unpack actual)
+                   ("mined block" `C8.isInfixOf` actual)
