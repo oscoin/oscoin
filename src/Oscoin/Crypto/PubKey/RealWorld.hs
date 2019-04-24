89d3b94d71a12730d5d51df229ef3ad17432a9d1
Alfredo Di Napoli <alfredo@monadic.xyz>
2019-04-24 16:04:33 +0200

    Exploratory session to use Ed25519

--- src/Oscoin/Crypto/PubKey/RealWorld.hs
+++ src/Oscoin/Crypto/PubKey/RealWorld.hs
@@ -11,11 +11,10 @@ import qualified Codec.Serialise.Decoding as CBOR
 import qualified Codec.Serialise.Encoding as CBOR
 import           Codec.Serialise.JSON (deserialiseParseJSON, serialiseToJSON)
 import           Control.Monad.Fail (fail)
-import qualified Crypto.PubKey.ECC.ECDSA as ECDSA
-import           Crypto.PubKey.ECC.Generate (generate)
-import           Crypto.PubKey.ECC.Types (CurveName(SEC_p256k1), getCurveByName)
+import           Crypto.Error (eitherCryptoError)
+import qualified Crypto.PubKey.Ed25519 as Ed25519
 import           Data.Aeson hiding (decode, encode)
-import           Data.ByteArray (ByteArrayAccess(..))
+import           Data.ByteArray (ByteArrayAccess(..), convert)
 import qualified Data.ByteString.Lazy as LBS
 import qualified Data.Hashable as H
 import           Oscoin.Crypto (Crypto)
@@ -27,25 +26,26 @@ import           Oscoin.Crypto.PubKey.Internal (PK(..), SK(..))
 instance HasDigitalSignature Crypto where
 
     newtype PublicKey Crypto =
-        PublicKey (PK Crypto ECDSA.PublicKey) deriving (Show, Eq, Ord)
+        PublicKey (PK Crypto Ed25519.PublicKey) deriving (Show, Eq, Ord)
 
-    newtype PrivateKey Crypto = PrivateKey (SK ECDSA.PrivateKey)
+    newtype PrivateKey Crypto = PrivateKey (SK Ed25519.SecretKey)
 
     newtype Signature Crypto =
-        Signature ECDSA.Signature deriving (Show, Eq)
+        Signature Ed25519.Signature deriving Show
 
     sign (PrivateKey (SK sk)) bytes =
-        let (alg :: HashAlgorithm Crypto) = hashAlgorithm
-         in Signed bytes . Signature <$> ECDSA.sign sk alg bytes
+        pure $ Signed bytes . Signature . Ed25519.sign sk (Ed25519.toPublic sk) $ bytes
 
     verify (PublicKey (PK pk _)) (Signed bytes (Signature sig)) =
-        let (alg :: HashAlgorithm Crypto) = hashAlgorithm
-         in ECDSA.verify alg pk sig bytes
+        Ed25519.verify pk bytes sig
 
     generateKeyPair = do
-        (pk, sk) <- generate (getCurveByName SEC_p256k1)
-        pure (PublicKey $ PK pk (hash pk), PrivateKey $ SK sk)
+        sk <- Ed25519.generateSecretKey
+        let pk = Ed25519.toPublic sk
+        pure (PublicKey $ PK pk (toHashed . hashByteArray $ pk), PrivateKey $ SK sk)
 
+instance Eq (Signature Crypto) where
+    (Signature s1) == (Signature s2) = s1 == s2
 
 {------------------------------------------------------------------------------
   Various instances
@@ -53,7 +53,7 @@ instance HasDigitalSignature Crypto where
 
 instance Ord (Signature Crypto) where
     (Signature a) <= (Signature b) =
-        (ECDSA.sign_r a, ECDSA.sign_s a) <= (ECDSA.sign_r b, ECDSA.sign_s b)
+        BA.convert @_ @ByteString a <= BA.convert @_ @ByteString b
 
 instance ToJSON (Signature Crypto) where
     toJSON = serialiseToJSON
@@ -62,23 +62,25 @@ instance FromJSON (Signature Crypto) where
     parseJSON = deserialiseParseJSON
 
 instance Serialise (Signature Crypto) where
-    encode (Signature ecdsa) =
-           CBOR.encodeListLen 3
+    encode (Signature ed25519) =
+           CBOR.encodeListLen 2
         <> CBOR.encodeWord 0
-        <> CBOR.encodeInteger (ECDSA.sign_r ecdsa)
-        <> CBOR.encodeInteger (ECDSA.sign_s ecdsa)
+        <> CBOR.encodeBytes (BA.convert ed25519)
 
     decode = do
         pre <- liftA2 (,) CBOR.decodeListLen CBOR.decodeWord
         case pre of
-            (3, 0) -> Signature <$>
-                liftA2 ECDSA.Signature CBOR.decodeInteger CBOR.decodeInteger
-            _ -> fail "CBOR Signature: invalid ECDSA signature"
+            (2, 0) -> do
+                sigE <- Ed25519.signature <$> CBOR.decodeBytes
+                case eitherCryptoError sigE of
+                  Left e -> fail ("CBOR signature conversion failed: " ++ show e)
+                  Right s -> pure $ Signature s
+            _      -> fail "CBOR Signature: invalid ECDSA signature"
 
-instance Eq (PK Crypto ECDSA.PublicKey) where
+instance Eq (PK Crypto Ed25519.PublicKey) where
     (PK a1 b1) == (PK a2 b2) = a1 == a2 && b1 == b2
 
-instance Ord (PK Crypto ECDSA.PublicKey) where
+instance Ord (PK Crypto Ed25519.PublicKey) where
     compare (PK _ b1) (PK _ b2) = b1 `compare` b2
 
 instance H.Hashable (PublicKey Crypto) where
@@ -89,8 +91,12 @@ instance ByteArrayAccess (PublicKey Crypto) where
     withByteArray pk f = withByteArray (LBS.toStrict (serialise pk)) f
 
 instance Serialise (PublicKey Crypto) where
-    encode (PublicKey (PK pk _)) = encode pk
-    decode = (\pk -> PublicKey $ PK @Crypto pk (hash pk)) <$> decode
+    encode (PublicKey (PK pk _)) = CBOR.encodeBytes (BA.convert pk)
+    decode = do
+        pkE <- Ed25519.publicKey <$> CBOR.decodeBytes
+        case eitherCryptoError pkE of
+            Left e   -> fail ("CBOR PublicKey conversion failed: " ++ show e)
+            Right pk -> pure $ PublicKey $ PK @Crypto pk (toHashed . hashByteArray $ pk)
 
 instance Hashable Crypto (PublicKey Crypto) where
     hash (PublicKey (PK _ h)) = toHashed $ fromHashed h
@@ -109,10 +115,9 @@ instance FromJSON (PublicKey Crypto) where
 serialisePrivateKey :: PrivateKey Crypto -> LBS.ByteString
 serialisePrivateKey (PrivateKey (SK sk)) =
     CBOR.toLazyByteString $
-       CBOR.encodeListLen 3
+       CBOR.encodeListLen 2
     <> CBOR.encodeWord 0
-    <> encode (ECDSA.private_curve sk)
-    <> CBOR.encodeInteger (ECDSA.private_d sk)
+    <> CBOR.encodeBytes (BA.convert sk)
 
 deserialisePrivateKey
     :: LBS.ByteString
@@ -122,6 +127,9 @@ deserialisePrivateKey bs = second snd $ CBOR.deserialiseFromBytes decoder bs
     decoder = do
         pre <- liftA2 (,) CBOR.decodeListLen CBOR.decodeWord
         case pre of
-            (3, 0) ->
-                PrivateKey . SK <$> (ECDSA.PrivateKey <$> decode <*> CBOR.decodeInteger)
+            (2, 0) -> do
+                skE <- Ed25519.secretKey <$> CBOR.decodeBytes
+                case eitherCryptoError skE of
+                  Left e -> fail ("CBOR SecretKey conversion failed: " ++ show e)
+                  Right sk -> pure . PrivateKey $ SK sk
             _ -> fail "CBOR: Invalid SK"
