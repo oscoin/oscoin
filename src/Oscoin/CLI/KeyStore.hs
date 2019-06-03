7220beaf33dd1c8bc229eea77da4dbec64e6b8be
Thomas Scholtes <thomas@monadic.xyz>
2019-06-03 09:45:30 +0200

    Reusable keypair file storage
    We extract the functionality to read and write key paris to disk from
    `Oscoin.CLI.KeyStore` into `Oscoin.Crypto.PubKey.FileStore`. This makes
    this functionality easier to use since we are providing just functions
    instead of an MTL-style type class.
    
    This is a necessary step towards having separate key management for the
    node keys and the account keys (#578).

--- src/Oscoin/CLI/KeyStore.hs
+++ src/Oscoin/CLI/KeyStore.hs
@@ -5,25 +5,17 @@
 -- deterimine the directory to store keys in.
 module Oscoin.CLI.KeyStore
     ( MonadKeyStore(..)
-    , KeyStoreError(..)
     ) where
 
 import           Oscoin.Crypto
 import           Oscoin.Prelude
-import qualified Prelude
 
 import qualified Oscoin.Crypto.Hash as Crypto
 import qualified Oscoin.Crypto.PubKey as Crypto
-import qualified Oscoin.Crypto.PubKey.RealWorld as Crypto.RealWorld
+import qualified Oscoin.Crypto.PubKey.FileStore as Crypto
 
-import           Codec.Serialise
-                 (DeserialiseFailure, deserialiseOrFail, serialise)
-import           Control.Monad.Except (liftEither)
-import qualified Data.ByteString as BS
-import qualified Data.ByteString.Lazy as LBS
 import           System.Directory
 import           System.FilePath
-import           System.IO.Error (isDoesNotExistError)
 
 
 class (Crypto.HasHashing c, Monad m) => MonadKeyStore c m | m -> c where
@@ -55,67 +47,17 @@ getConfigPath :: MonadIO m => Maybe FilePath -> m FilePath
 getConfigPath Nothing         = liftIO . getXdgDirectory XdgConfig $ "oscoin"
 getConfigPath (Just userPath) = pure userPath
 
-getSecretKeyPath :: Maybe FilePath -> IO FilePath
-getSecretKeyPath mbFilePath = flip (</>) "id.key" <$> getConfigPath mbFilePath
-
-getPublicKeyPath :: Maybe FilePath -> IO FilePath
-getPublicKeyPath mbFilePath = flip (</>) "id.pub" <$> getConfigPath mbFilePath
-
-ensureConfigDir :: MonadIO m => Maybe FilePath -> m ()
-ensureConfigDir mbUserPath = do
-    configDir <- getConfigPath mbUserPath
-    liftIO $ createDirectoryIfMissing True configDir
-
-data KeyStoreError =
-    -- | Key file at specified path doesn't exist.
-      KeyNotFound FilePath
-    -- | A key couldn't be deserialised correctly. The first
-    -- argument is the path to the key on disk.
-    | KeyDeserialisationFailure FilePath DeserialiseFailure
-
-instance Show KeyStoreError where
-    show = \case
-        KeyNotFound configPath ->
-            "Key file doesn't exist: " <> configPath
-
-        KeyDeserialisationFailure configPath cborError ->
-            mconcat [ "Loading the key from "
-                    , configPath
-                    , " failed. The reason was: "
-                    , show cborError
-                    ]
-
-instance Exception KeyStoreError
-
 instance MonadKeyStore Crypto (ReaderT (Maybe FilePath) IO) where
     keysPath = ask
-    writeKeyPair (pk, sk) =  do
+    writeKeyPair keyPair =  do
         mbUserPath <- keysPath
-        ensureConfigDir mbUserPath
-        liftIO $ do
-            skPath <- getSecretKeyPath mbUserPath
-            LBS.writeFile skPath $ Crypto.RealWorld.serialisePrivateKey sk
-            pkPath <- getPublicKeyPath mbUserPath
-            LBS.writeFile pkPath $ serialise pk
+        keyDir <- getConfigPath mbUserPath
+        liftIO $ createDirectoryIfMissing True keyDir
+        let keyPrefix = keyDir </> "id"
+        Crypto.writeKeyPair keyPrefix keyPair
 
     readKeyPair = do
         mbUserPath <- keysPath
-        liftIO $ do
-            skPath <- getSecretKeyPath mbUserPath
-            pkPath <- getPublicKeyPath mbUserPath
-            keys   <-
-                runExceptT $ liftA2 (,)
-                    (readFileLbs pkPath >>=
-                        liftEither
-                            . first (KeyDeserialisationFailure pkPath)
-                            . deserialiseOrFail)
-                    (readFileLbs skPath >>=
-                        liftEither
-                            . first (KeyDeserialisationFailure skPath)
-                            . Crypto.RealWorld.deserialisePrivateKey)
-            either throwM pure keys
-      where
-        readFileLbs path = ExceptT $
-            map (first . const $ KeyNotFound path)
-                . tryJust (guard . isDoesNotExistError)
-                $ LBS.fromStrict <$> BS.readFile path
+        keyDir <- getConfigPath mbUserPath
+        let keyPrefix = keyDir </> "id"
+        Crypto.readKeyPair keyPrefix
